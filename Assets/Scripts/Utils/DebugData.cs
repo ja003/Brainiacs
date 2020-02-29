996af1368c518b66cc00b2bc763dafd9cd48b302
Adam Jurík <ja004@seznam.cz>
2020-02-29 13:41:18 +0100

    18 - Hero colors
    - removed old textures
    - replaced color palettes for 64b
    - replaced character textures for 64b

--- Assets/Scripts/Utils/DebugData.cs
+++ Assets/Scripts/Utils/DebugData.cs
@@ -28,7 +28,7 @@ public static class DebugData
 		{
 			case 1:
 				player = new PlayerInitInfo(
-			EHero.Tesla, GetPlayerName(pIndex), EPlayerColor.Green);
+			EHero.Nobel, GetPlayerName(pIndex), EPlayerColor.Green);
 				player.PlayerKeys = new PlayerKeys(
 					KeyCode.UpArrow, KeyCode.RightArrow,
 					KeyCode.DownArrow, KeyCode.LeftArrow,
@@ -36,7 +36,7 @@ public static class DebugData
 				break;
 			case 2:
 				player = new PlayerInitInfo(
-			EHero.Nobel, GetPlayerName(pIndex), EPlayerColor.Pink);
+			EHero.Tesla, GetPlayerName(pIndex), EPlayerColor.Pink);
 				player.PlayerKeys = new PlayerKeys(
 					KeyCode.W, KeyCode.D, KeyCode.S, KeyCode.A,
 					KeyCode.LeftControl, KeyCode.LeftShift);
